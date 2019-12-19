91077fb107a61abe1d4ae37e272e18c94117b038
BarenneM <barenne.marie@outlook.com>
2019-12-19 15:08:31 +0100

    Serveur5

--- Serveur.py
+++ Serveur.py
@@ -10,9 +10,9 @@ print("Le serveur écoute à présent sur le port {}".format(port))
 
 connexion_avec_client, infos_connexion = connexion_principale.accept()
 
-num1 = connexion_avec_client.recv (1).decode()
-num2 = connexion_avec_client.recv (1).decode()
-op = connexion_avec_client.recv (1).decode()
+num1 = float(connexion_avec_client.recv (1).decode())
+num2 = float(connexion_avec_client.recv (1).decode())
+op = int(connexion_avec_client.recv (1).decode())
 
 if op == 1 :
     connexion_avec_client.send(num1 + num2).encode()
@@ -27,7 +27,7 @@ elif op == 4 :
     connexion_avec_client.send(num1 / num2).encode()
 
 else :
-    connexion_avec_client.send("Donnée erronnée").encode()
+    print(connexion_avec_client.send("Donnée erronnée").encode())
 
 print("Fermeture de la connexion")
 connexion_avec_client.close()
