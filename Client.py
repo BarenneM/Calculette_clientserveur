845760b05753d310a4fdeafb7ee16efd607fb7d4
BarenneM <barenne.marie@outlook.com>
2019-12-19 13:54:57 +0100

    Client2

--- Client.py
+++ Client.py
@@ -5,10 +5,10 @@ port = 12800
 
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_serveur.connect((hote, port))
-print("Connexion établie avec le serveur sur le port {}".format(port))
+print ("Connexion établie avec le serveur sur le port {}".format(port))
 
-connexion_avec_serveur.num1 = float(input("Entrer le premier numéro :"))
-connexion_avec_serveur.num2 = float(input("Entrer le deuxième numéro : "))
+num1 = float(input("Entrer le premier numéro :"))
+num2 = float(input("Entrer le deuxième numéro : "))
 
 connexion_avec_serveur.send = num1 
 connexion_avec_serveur.send = num2
@@ -22,7 +22,7 @@ print("4- Division")
 op = input("Entrer votre choix (1 ; 2 ; 3 ou 4): ")
 connexion_avec_serveur.send = op
 
-connexion_avec_serveur.reciv (1)
+connexion_avec_serveur.recv (1)
 connexion_avec_serveur.send = b"fin"
 
 print("Fermeture de la connexion")
